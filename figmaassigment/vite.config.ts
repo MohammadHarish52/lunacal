0f08b96a3974b6853358390f949a72ce689fae81
tachyonok <tachyonparticle123@gmail.com>
2024-09-10 03:29:18 +0530

    deploymwnt fixes

--- figmaassigment/vite.config.ts
+++ figmaassigment/vite.config.ts
@@ -1,6 +1,10 @@
+/// <reference types="node" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
